394d52a4563815f4edf62df2abd75d67adc79051
NaveenkumarChowki <51572193+NaveenkumarChowki@users.noreply.github.com>
2019-06-24 11:27:30 +0530

    Add files via upload

---
+++ src/app/authentication-module/login-component/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material';
+import {FormGroup,FormBuilder,Validators} from '@angular/forms'
+
+import { Router } from '@angular/router';
+import { LoginModel } from 'src/app/Model/login.model';
+
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  password:String;
+  email:String;
+
+  user: LoginModel;
+  loginForm: FormGroup;
+  hide = true;
+
+  constructor(private router: Router,private formBuilder: FormBuilder) { }
+    
+
+  ngOnInit() {
+    this.user = new LoginModel();
+    this.loginForm = this.formBuilder.group({
+      'email': [this.user.email, [
+        Validators.required,
+        Validators.email
+      ]],
+      'password': [this.user.password, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30)
+      ]]
+    });
+  }
+
+  login() : void {
+    
+    if(this.loginForm.value.email == "naveen@gmail.com" && this.loginForm.value.password == "123456"){
+    
+     this.router.navigate(["search"]);
+    }else {
+      alert("Invalid credentials");
+    }
+  }
+  onLoginSubmit() {
+    alert(this.user.email + ' ' + this.user.password);
+  }
+}
